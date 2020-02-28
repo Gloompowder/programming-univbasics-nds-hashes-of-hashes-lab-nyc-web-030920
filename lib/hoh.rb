c54137d9a3217087b3d4b6e62b7873e4e1842134
William Lin <willlin1996@gmail.com>
2020-02-28 00:43:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,17 +45,18 @@ def naming_system
     label: "Kingdom",
   sub_category: {
     label: "Phylum",
-    sub_category: {
-      label: "Class",
-      sub_category: {
-  label: "Order",
   sub_category: {
-  label: "Family",
+    label: "Class",
+  sub_category: {
+    label: "Order",
+  sub_category: {
+    label: "Family",
   sub_category: {
     label: "Genus",
-    sub_category: {
+  sub_category: {
   label: "Species",
-  sub_category: {}
+    sub_category: {}
+  }
             }
           }
         }
